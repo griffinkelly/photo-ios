8b32884401e687073a96d6c2e448f4641ec76f7e
nschulze <nschulze16@gmail.com>
2016-05-31 12:06:35 -0500

    Initial Commit

---
+++ trac-photos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  trac-photos
+//
+//  Created by Nick Schulze on 5/31/16.
+//  Copyright © 2016 TRAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
